be266a110fbc40f212b935e6876d68893bf9ae25
bruno <bruno.costa1203@yahoo.com>
2023-02-13 20:15:21 -0300

    feat(components/newsletter): create a section newsletter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import "./App.css";
-
+import Newsletter from './components/newsletter/Newsletter';
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Newsletter />
+    </>
+  );
 }
 
 export default App;
