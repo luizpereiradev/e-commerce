c8e35cf83c85d914c7c5057a788348e32c216f94
KhallylReisFonseca <khallylreis@gmail.com>
2023-02-18 19:58:51 -0300

    feat(form): create responsive form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import "./App.css";
+import Form from "./components/form/Form";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return(
+  <>
+  <Form />
+  </>
+  )
 }
 
 export default App;
