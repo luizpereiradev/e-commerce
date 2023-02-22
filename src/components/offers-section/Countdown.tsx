33cc7a0c4f3c8ea66e317b6d24d8ed18874422e6
Luiz Felipe <luckpecontato@gmail.com>
2023-02-22 15:19:47 -0300

    feat(countdown): merge contdown function

--- src/components/offers-section/Countdown.tsx
+++ src/components/offers-section/Countdown.tsx
@@ -15,7 +15,9 @@ export default function Countdown() {
     let interval: ReturnType<typeof setInterval>;
     if (isActive) {
       interval = setInterval(() => {
-        if (seconds > 0) { setSeconds(seconds - 1); }
+        if (seconds > 0) {
+          setSeconds(seconds - 1);
+        }
         if (seconds === 0) {
           if (minutes === 0) {
             if (hours === 0) {
@@ -47,35 +49,33 @@ export default function Countdown() {
 
   return (
     <div className="lg:w-full sm:text-base text-sm w-1/2 lg:justify-start justify-end sm:h-16 h-12 lg:h-12 grid grid-flow-col gap-1 text-center auto-cols-max mr-3 lg:mr-0">
-      
-      <div className="rounded-lg gap-1 sm:w-16 items-center sm:justify-center lg:w-12 flex w-12 flex-col p-2 bg-gray-600 text-gray-100">
+      <div className="rounded-lg gap-1 sm:w-16 items-center sm:justify-center lg:w-12 flex w-12 flex-col p-2 bg-gray-200 text-gray-700">
         <span className=" countdown font-mono">
           <span style={dias} className="font-bold"></span>
         </span>
         <span className="text-xs">days</span>
       </div>
-      
-      <div className="rounded-lg gap-1 sm:w-16 items-center sm:justify-center lg:w-12 flex w-12 flex-col p-2 bg-gray-600 text-gray-100">
+
+      <div className="rounded-lg gap-1 sm:w-16 items-center sm:justify-center lg:w-12 flex w-12 flex-col p-2  bg-gray-200 text-gray-700">
         <span className=" countdown font-mono">
           <span style={horas} className="font-bold"></span>
         </span>
         <span className="text-xs">hours</span>
       </div>
-      
-      <div className="rounded-lg gap-1 sm:w-16 sm:justify-center lg:w-12 items-center w-12 flex flex-col p-2 bg-gray-600 text-gray-100">
+
+      <div className="rounded-lg gap-1 sm:w-16 sm:justify-center lg:w-12 items-center w-12 flex flex-col p-2  bg-gray-200 text-gray-700">
         <span className="countdown font-mono">
           <span style={minutos} className="font-bold"></span>
         </span>
         <span className="text-xs">min</span>
       </div>
-      
-      <div className="rounded-lg gap-1 sm:w-16 sm:justify-center items-center lg:w-12 w-12 flex flex-col p-2 bg-gray-600 text-gray-100">
+
+      <div className="rounded-lg gap-1 sm:w-16 sm:justify-center items-center lg:w-12 w-12 flex flex-col p-2 bg-gray-200 text-gray-700">
         <span className="countdown font-mono">
           <span style={segundos} className="font-bold"></span>
         </span>
         <span className="text-xs">sec</span>
       </div>
-    
     </div>
   );
 }
