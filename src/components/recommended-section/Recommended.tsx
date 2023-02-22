82747592d8a49ffde73842efe6cbea71fee6c553
Luiz Felipe <luckpecontato@gmail.com>
2023-02-22 14:51:24 -0300

    feat(recommendedcard): fetch function

--- src/components/recommended-section/Recommended.tsx
+++ src/components/recommended-section/Recommended.tsx
@@ -1,15 +1,24 @@
+import { useEffect, useState } from "react";
+import { getProductsFromCategoryAndQuery } from "../../services/categories";
 import RecommendedCard from "./RecommendedCard";
 
 export default function Recommended() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProductsFromCategoryAndQuery("MLB1000").then((data: { results: [] }) => {
+      setProducts(data.results);
+    });
+  }, []);
   return (
     <div>
       <h1 className="font-medium text-xl p-4 lg:px-0">Recommended items</h1>
       <div className="grid gap-3 lg:gap-x-5 w-full grid-auto-fit-[165px] lg:grid-auto-fit-[240px]">
-      {Array(8)
-        .fill("")
-        .map((_,x) => (
-            <RecommendedCard key={`${x}recommended`}/>
-            ))}
+        {products
+        .slice(0, 8)
+        .map((product, x) => (
+          <RecommendedCard product={product} key={`${x}recommended`} />
+        ))}
       </div>
     </div>
   );
--- src/components/recommended-section/RecommendedCard.tsx
+++ src/components/recommended-section/RecommendedCard.tsx
@@ -1,15 +1,19 @@
-export default function RecommendedCard() {
+export default function RecommendedCard({ product }: { product: {
+  thumbnail: string;
+  title: string;
+  price: number;
+}}) {
   return (
     <div className="bg-white h-60 w-[85%] m-auto border-2 rounded p-2 pb-5 lg:w-full lg:h-[300px] lg:m-0">
       <div className="flex flex-col justify-between h-full">
         <img
-          src="http://http2.mlstatic.com/D_854001-MLB51230324147_082022-O.jpg"
+          src={product.thumbnail}
           alt="product"
           className="max-h-28 lg:max-h-32 m-auto"
         />
         <div className="flex flex-col gap-1 px-2">
-        <p className="text-black font-bold">$499.90</p>
-        <p className="text-gray-400"> Iphone black blue wallet travel</p>
+        <p className="text-black font-bold">R${product.price}</p>
+        <p className="text-gray-400 truncate"> {product.title} </p>
         </div>
       </div>
     </div>
