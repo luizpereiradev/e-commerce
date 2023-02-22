682f82f26ea3a32f7c716073bc473c771a7108ac
Luiz Felipe <luckpecontato@gmail.com>
2023-02-22 15:39:21 -0300

    fix(productdetails): type fix

--- global.d.ts
+++ global.d.ts
@@ -3,5 +3,7 @@ declare module "react" {
   // augment CSSProperties here
   interface CSSProperties {
     "--value"?: string | number;
+    "--swiper-navigation-color": string;
+    "--swiper-pagination-color": string;
   }
 }
