8bc24fd900b44bbd6f5b92fbf6752cf1e7f51270
Luiz Pereira <luckpecontato@gmail.com>
2023-07-22 16:12:14 -0300

    docs(server): swagger added

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,13 +1,18 @@
 import express from 'express';
 import Factory from './factory';
 import Middlewares from './middlewares/User';
+import swaggerUi from 'swagger-ui-express'
+
+import swaggerDocument from '../swagger.json'
 
 const app = express();
 
 app.use(express.json());
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
 app.use('/user', Factory.userRouter);
-app.use('/category', Factory.categoryRouter);
+app.use('/categories', Factory.categoryRouter);
 
 app.use(Middlewares.error);
 
