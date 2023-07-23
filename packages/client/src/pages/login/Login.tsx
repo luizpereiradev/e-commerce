9e7ef8625a45f824fc3bf0e9994e3e7f29db582b
Luiz Pereira <luckpecontato@gmail.com>
2023-07-23 18:36:13 -0300

    feat(client): login screen styling

--- packages/client/src/pages/login/Login.tsx
+++ packages/client/src/pages/login/Login.tsx
@@ -1,7 +1,6 @@
 import { useForm, SubmitHandler } from "react-hook-form";
 import axios from "axios";
 import { useState } from "react";
-import Input from "../../components/Input";
 
 type Inputs = {
   email: string;
@@ -19,7 +18,7 @@ function Login() {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<Inputs>({ mode: "all" });
+  } = useForm<Inputs>({ mode: "onBlur" });
 
   const [ResponseError, setResponseError] = useState<ResponseError>();
 
@@ -43,12 +42,50 @@ function Login() {
 
   // TODO: add error message
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Input label="email" name="email" register={register} />
+    <div className="flex flex-col items-center justify-center w-screen h-screen bg-gray-200 text-gray-700">
+      <h1 className="font-bold text-2xl mb-10 ">Welcome Back :)</h1>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col bg-white rounded shadow-lg p-12 mt-12"
+      >
+        <label className="font-semibold text-xs" htmlFor="usernameField">
+          Email
+        </label>
+        <input
+          {...register("email", { required: true })}
+          className="flex items-center h-12 px-4 w-64 bg-gray-200 mt-2 rounded focus:outline-none focus:ring-2"
+          type="text"
+        />
+        <span className={`text-red-500 text-xs my-1 self-end ${!errors.email && 'opacity-0'}`}>
+          This field is required
+        </span>
+        <label className="font-semibold text-xs" htmlFor="passwordField">
+          Password
+        </label>
 
-      <Input label="password" name="password" register={register} />
-      <input type="submit" />
-    </form>
+        <input
+          {...register("password", { required: true })}
+          className="flex items-center h-12 px-4 w-64 bg-gray-200 mt-2 rounded focus:outline-none focus:ring-2"
+          type="password"
+        />
+        <span className={`text-red-500 text-xs my-1 self-end ${!errors.password && 'opacity-0'}`}>
+          This field is required
+        </span>
+
+        <button className="flex items-center justify-center h-12 px-6 w-64 bg-blue-600 mt-4 rounded font-semibold text-sm text-blue-100 hover:bg-blue-700">
+          Login
+        </button>
+        <div className="flex mt-6 justify-center text-xs">
+          <a className="text-blue-400 hover:text-blue-500" href="#">
+            Forgot Password
+          </a>
+          <span className="mx-2 text-gray-300">/</span>
+          <a className="text-blue-400 hover:text-blue-500" href="#">
+            Sign Up
+          </a>
+        </div>
+      </form>
+    </div>
   );
 }
 
